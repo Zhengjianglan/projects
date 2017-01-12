d9415000d12996b995bddca3000244f6edf52245
Zhengjianglan <414704822@qq.com>
2017-01-12 22:55:49 +0800

    rm

--- homePro/go_spider/core/common/com_interfaces/com_interfaces.go
+++
@@ -1,8 +0,0 @@
-// Package com_interfaces contains some common interface of GO_SPIDER project.
-package com_interfaces
-
-// The Task represents interface that contains environment variables.
-// It inherits by Spider.
-type Task interface {
-    Taskname() string
-}
